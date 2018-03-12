98bfae140f20d0407243878e5fe6d7bd601eedc6
chenqianyi <chen.qianyi@immomo.com>
2018-03-12 15:07:46 +0800

    source map

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -54,11 +54,11 @@ module.exports = {
   performance: {
     hints: "warning"
   },
-  devtool: '#eval-source-map'
+  // devtool: '#eval-source-map'
 };
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map';
+  // module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
